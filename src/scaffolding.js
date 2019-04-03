69423d746fba92c651b98201a6db52515738cf3e
Keshav kumar <keshav12raju@gmail.com>
2019-04-03 17:40:17 +0530

    scaffolding with folder name

--- src/scaffolding.js
+++ src/scaffolding.js
@@ -2,11 +2,23 @@
 
 var ncp = require('ncp').ncp;
 const path = require("path");
+const yargs = require("yargs");
 
 const basePath = process.cwd();
 
+// run mock-api-scaffold --folder_name="desired_folder_name"
+
+let argv = require("yargs").options({
+  "folder_name": {
+    alias: "fn",
+    describe: "Name of the folder",
+    default: "mock-server",
+    type: "string"
+  }
+}).parse();
+
 const source = path.join(__dirname, "../scaffolding");
-const destination = path.join(basePath, "mock-server");
+const destination = path.join(basePath, argv.folder_name);
 
 ncp.limit = 16;
 ncp(source, destination, function (err) {
