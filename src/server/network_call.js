aac370b0b633dbf493b3cb711a55f6eab4f630bc
Keshav Kumar <keshav12raju@gmail.com>
2020-11-05 11:00:27 +0530

    removed request header and added chalk

--- src/server/network_call.js
+++ src/server/network_call.js
@@ -1,5 +1,7 @@
 const URL = require("url").URL;
 const superagent = require("superagent");
+const _isEmpty = require("lodash.isempty");
+const chalk = require("chalk");
 
 const networkCall = function (parameters) {
   let {method, url: req_url, headers ={}, query={}, hostname, payload} = parameters;
@@ -14,18 +16,18 @@ const networkCall = function (parameters) {
   return new Promise(function (resolve, reject) {
     superagent[method](constructed_url.href)
     .set(headers)
-    .send(payload)
+    .send(!_isEmpty(payload)? payload: null)
     .query(query)
     .then(function (response_data) {
-      console.debug(JSON.stringify({method, constructed_url, payload, query}));
+      console.log(chalk.green(JSON.stringify({method, constructed_url, payload, query})));
       resolve(response_data.body);
     })
     .catch(function (err) {
-      console.debug(JSON.stringify({method, constructed_url, payload, query, headers}))
+      console.log(chalk.red(JSON.stringify({method, constructed_url, payload, query, headers})));
       let error_text = err.response && err.response.text;
       let error_status = err.status;
       if (!error_text || !error_status) {
-        console.debug("err", err);
+        console.log(chalk.redBright("err", err));
         reject();
       };
       reject({
