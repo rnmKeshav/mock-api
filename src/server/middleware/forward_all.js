b58f6ca7f6280bfc50ed2ba0b27f6f89b7eeda8d
Vanshaj Girotra <vanshaj.g@practo.com>
2020-11-23 00:15:24 +0530

    fix path for url'

--- src/server/middleware/forward_all.js
+++ src/server/middleware/forward_all.js
@@ -1,7 +1,7 @@
 var networkCall = require("../network_call");
 
 const forwardAll = function (req, res, next) {
-  let { method, _parsedUrl: { pathname }, app: { locals: { config } }, query: req_query, headers: req_headers, body } = req;
+  let { method, path: pathname, app: { locals: { config } }, query: req_query, body } = req;
   let { forward: { hostname, query: custom_query, headers: custom_headers } } = config;
   let query = Object.assign({}, req_query, custom_query);
   let headers = Object.assign({}, custom_headers);
